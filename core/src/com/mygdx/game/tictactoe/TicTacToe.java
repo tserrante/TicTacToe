e6da7bc11515fae38e831667c5058c8a5741cd48
TS <tserrante@gmail.com>
2021-10-21 19:07:21 -0400

    Add gameMusic, objects for camera and sprites

--- core/src/com/mygdx/game/tictactoe/TicTacToe.java
+++ core/src/com/mygdx/game/tictactoe/TicTacToe.java
@@ -4,15 +4,23 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
 public class TicTacToe extends ApplicationAdapter
 {
+	// music and textures
 	private Texture piece_o;
 	private Texture piece_x;
 	private Texture board;
 	private Sound placedPiece;
 	private Sound roundOver;
+	private Music gameMusic;
+	// camera and spritebatch
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 
 	@Override
 	public void create()
@@ -25,7 +33,18 @@ public class TicTacToe extends ApplicationAdapter
 		// load the sound effects for placing a piece and ending a round
 		placedPiece = Gdx.audio.newSound(Gdx.files.internal("clack.wav"));
 		roundOver = Gdx.audio.newSound(Gdx.files.internal("bell.wav"));
+		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("gameMusic.mp3"));
 
+		// loop game music
+		gameMusic.setLooping(true);
+		gameMusic.play();
+
+		// create the camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		// create the sprite batch
+		batch = new SpriteBatch();
 
 	}
 }
