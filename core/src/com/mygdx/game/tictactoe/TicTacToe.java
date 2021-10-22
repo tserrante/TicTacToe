b9633d698e058f073f65747da5c246f16e059705
TS <tserrante@gmail.com>
2021-10-21 21:04:07 -0400

    change format of bell and clack-clunk to mp3, added batch drawing and mouse input

--- core/src/com/mygdx/game/tictactoe/TicTacToe.java
+++ core/src/com/mygdx/game/tictactoe/TicTacToe.java
@@ -7,32 +7,40 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class TicTacToe extends ApplicationAdapter
 {
 	// music and textures
-	private Texture piece_o;
-	private Texture piece_x;
-	private Texture board;
-	private Sound placedPiece;
-	private Sound roundOver;
+	private Texture image_o;
+	private Texture image_x;
+	private Texture boardTexture;
+	private Sound placePieceSound;
+	private Sound roundOverSound;
 	private Music gameMusic;
 	// camera and spritebatch
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+	// rectangle object for textures
+	private Rectangle r_board;
+	private Rectangle r_piece_x;
+	private Rectangle r_piece_o;
+	// for mouse position
+	private Vector3 touchPos;
 
 	@Override
 	public void create()
 	{
 		// load the images for the board and game pieces
-		piece_o = new Texture(Gdx.files.internal("o.png"));
-		piece_x = new Texture(Gdx.files.internal("x.png"));
-		board = new Texture(Gdx.files.internal("board.png"));
+		image_o = new Texture(Gdx.files.internal("o.png"));
+		image_x = new Texture(Gdx.files.internal("x.png"));
+		boardTexture = new Texture(Gdx.files.internal("board.png"));
 
 		// load the sound effects for placing a piece and ending a round
-		placedPiece = Gdx.audio.newSound(Gdx.files.internal("clack.wav"));
-		roundOver = Gdx.audio.newSound(Gdx.files.internal("bell.wav"));
+		placePieceSound = Gdx.audio.newSound(Gdx.files.internal("bell.mp3"));
+		roundOverSound = Gdx.audio.newSound(Gdx.files.internal("bell.mp3"));
 		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("gameMusic.mp3"));
 
 		// loop game music
@@ -46,5 +54,71 @@ public class TicTacToe extends ApplicationAdapter
 		// create the sprite batch
 		batch = new SpriteBatch();
 
+		// setup the board
+		r_board = new Rectangle();
+		r_board.x = 800/2 - 64/2; // center horizontally
+		r_board.y = 480/2 - 64/2; // center vertically
+		r_board.width = 256;
+		r_board.height = 256;
+
+		// setup the pieces
+		r_piece_o = new Rectangle();
+		r_piece_o.x = 800/2 - 64/2;
+		r_piece_o.y = 50;
+		r_piece_o.width = 64;
+		r_piece_o.height = 64;
+
+		r_piece_x = new Rectangle();
+		r_piece_x.x = 800/2 + 64/2;
+		r_piece_x.y = 50;
+		r_piece_x.width = 64;
+		r_piece_x.height = 64;
+	}
+
+	// render the board
+	@Override
+	public void render()
+	{
+		// clear the screen
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		// update the camera, once per frame
+		camera.update();
+		// use the coordinates specified by the camera,
+		// where camera.combined is a matrix.
+		batch.setProjectionMatrix(camera.combined);
+		// sprite batch records all drawing commands between begin and end
+		batch.begin();
+		batch.draw(boardTexture, r_board.x, r_board.y);
+		batch.draw(image_o, r_piece_o.x, r_piece_o.y);
+		batch.draw(image_x, r_piece_x.x, r_piece_x.y);
+		batch.end();
+
+		if(Gdx.input.isTouched())
+		{
+			touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			// we will just worry about the x piece now
+			r_piece_x.x = touchPos.x - 64 / 2;
+			r_piece_x.y = touchPos.x - 64 / 2;
+
+		}
+
+		if(r_piece_x.overlaps(r_board))
+		{
+			placePieceSound.play();
+		}
+
+	}
+	@Override
+	public void dispose()
+	{
+		image_o.dispose();
+		image_x.dispose();
+		boardTexture.dispose();
+		placePieceSound.dispose();
+		roundOverSound.dispose();
+		gameMusic.dispose();
+		batch.dispose();
 	}
 }
