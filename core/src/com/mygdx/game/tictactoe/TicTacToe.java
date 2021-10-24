13e945ca64f8a7a4ab4f4a9fd54468a292015faf
ts <tserrante@gmail.com>
2021-10-23 20:25:40 -0400

    remove graphics related objects and member to start over

--- core/src/com/mygdx/game/tictactoe/TicTacToe.java
+++ core/src/com/mygdx/game/tictactoe/TicTacToe.java
@@ -14,29 +14,20 @@ import com.badlogic.gdx.math.Rectangle;
 public class TicTacToe extends ApplicationAdapter
 {
 	// music and textures
-	private Texture image_o;
-	private Texture image_x;
-	private Texture boardTexture;
+
 	private Sound placePieceSound;
 	private Sound roundOverSound;
 	private Music gameMusic;
 	// camera and spritebatch
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	// rectangle object for textures
-	private Rectangle r_board;
-	private Rectangle r_piece_x;
-	private Rectangle r_piece_o;
-	// for mouse position
-	private Vector3 touchPos;
+
+
+
 
 	@Override
 	public void create()
 	{
-		// load the images for the board and game pieces
-		image_o = new Texture(Gdx.files.internal("o.png"));
-		image_x = new Texture(Gdx.files.internal("x.png"));
-		boardTexture = new Texture(Gdx.files.internal("board.png"));
+
 
 		// load the sound effects for placing a piece and ending a round
 		placePieceSound = Gdx.audio.newSound(Gdx.files.internal("bell.mp3"));
@@ -51,28 +42,7 @@ public class TicTacToe extends ApplicationAdapter
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 
-		// create the sprite batch
-		batch = new SpriteBatch();
 
-		// setup the board
-		r_board = new Rectangle();
-		r_board.x = 800/2 - 64/2; // center horizontally
-		r_board.y = 480/2 - 64/2; // center vertically
-		r_board.width = 256;
-		r_board.height = 256;
-
-		// setup the pieces
-		r_piece_o = new Rectangle();
-		r_piece_o.x = 800/2 - 64/2;
-		r_piece_o.y = 50;
-		r_piece_o.width = 64;
-		r_piece_o.height = 64;
-
-		r_piece_x = new Rectangle();
-		r_piece_x.x = 800/2 + 64/2;
-		r_piece_x.y = 50;
-		r_piece_x.width = 64;
-		r_piece_x.height = 64;
 	}
 
 	// render the board
@@ -85,40 +55,22 @@ public class TicTacToe extends ApplicationAdapter
 		camera.update();
 		// use the coordinates specified by the camera,
 		// where camera.combined is a matrix.
-		batch.setProjectionMatrix(camera.combined);
+		//batch.setProjectionMatrix(camera.combined);
 		// sprite batch records all drawing commands between begin and end
-		batch.begin();
-		batch.draw(boardTexture, r_board.x, r_board.y);
-		batch.draw(image_o, r_piece_o.x, r_piece_o.y);
-		batch.draw(image_x, r_piece_x.x, r_piece_x.y);
-		batch.end();
 
-		if(Gdx.input.isTouched())
-		{
-			touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			// we will just worry about the x piece now
-			r_piece_x.x = touchPos.x - 64 / 2;
-			r_piece_x.y = touchPos.x - 64 / 2;
 
-		}
-
-		if(r_piece_x.overlaps(r_board))
-		{
+		//if(r_piece_x.overlaps(r_board))
+		//{
 			placePieceSound.play();
-		}
+		//}
 
 	}
 	@Override
 	public void dispose()
 	{
-		image_o.dispose();
-		image_x.dispose();
-		boardTexture.dispose();
 		placePieceSound.dispose();
 		roundOverSound.dispose();
 		gameMusic.dispose();
-		batch.dispose();
+
 	}
 }
