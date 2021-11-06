5097ee67aceb179f29d50689c67cd968f1fde04f
TS <tserrante@gmail.com>
2021-11-05 20:29:29 -0400

    change assets to multiples of 64.  draw board.png and x.png

--- core/src/com/mygdx/game/tictactoe/TicTacToe.java
+++ core/src/com/mygdx/game/tictactoe/TicTacToe.java
@@ -7,36 +7,45 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
 public class TicTacToe extends ApplicationAdapter
 {
+
 	// music and textures
-	private Texture image_o;
-	private Texture image_x;
-	private Texture boardTexture;
+	private Texture boardImage;
+	private Rectangle board;
+
+	private Texture pieceImage_x;
+	private Rectangle pieceX;
+
+
 	private Sound placePieceSound;
 	private Sound roundOverSound;
 	private Music gameMusic;
+
 	// camera and spritebatch
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	// rectangle object for textures
-	private Rectangle r_board;
-	private Rectangle r_piece_x;
-	private Rectangle r_piece_o;
-	// for mouse position
+
 	private Vector3 touchPos;
 
 	@Override
 	public void create()
 	{
 		// load the images for the board and game pieces
-		image_o = new Texture(Gdx.files.internal("o.png"));
-		image_x = new Texture(Gdx.files.internal("x.png"));
-		boardTexture = new Texture(Gdx.files.internal("board.png"));
+		boardImage = new Texture(Gdx.files.internal("board.png"));
+		board = new Rectangle();
+
+		board.x = (Gdx.graphics.getWidth() / 2) - (boardImage.getWidth() / 2);
+		board.y = (Gdx.graphics.getHeight() / 2) - (boardImage.getHeight() / 2);
+
+		pieceImage_x = new Texture(Gdx.files.internal("x.png"));
+		pieceX = new Rectangle();
 
 		// load the sound effects for placing a piece and ending a round
 		placePieceSound = Gdx.audio.newSound(Gdx.files.internal("bell.mp3"));
@@ -50,29 +59,9 @@ public class TicTacToe extends ApplicationAdapter
 		// create the camera
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
-
 		// create the sprite batch
 		batch = new SpriteBatch();
 
-		// setup the board
-		r_board = new Rectangle();
-		r_board.x = 800/2 - 64/2; // center horizontally
-		r_board.y = 480/2 - 64/2; // center vertically
-		r_board.width = 256;
-		r_board.height = 256;
-
-		// setup the pieces
-		r_piece_o = new Rectangle();
-		r_piece_o.x = 800/2 - 64/2;
-		r_piece_o.y = 50;
-		r_piece_o.width = 64;
-		r_piece_o.height = 64;
-
-		r_piece_x = new Rectangle();
-		r_piece_x.x = 800/2 + 64/2;
-		r_piece_x.y = 50;
-		r_piece_x.width = 64;
-		r_piece_x.height = 64;
 	}
 
 	// render the board
@@ -80,42 +69,34 @@ public class TicTacToe extends ApplicationAdapter
 	public void render()
 	{
 		// clear the screen
-		ScreenUtils.clear(0, 0, 0.2f, 1);
-		// update the camera, once per frame
+		ScreenUtils.clear(1, 1, 1, 1);
 		camera.update();
-		// use the coordinates specified by the camera,
-		// where camera.combined is a matrix.
 		batch.setProjectionMatrix(camera.combined);
-		// sprite batch records all drawing commands between begin and end
-		batch.begin();
-		batch.draw(boardTexture, r_board.x, r_board.y);
-		batch.draw(image_o, r_piece_o.x, r_piece_o.y);
-		batch.draw(image_x, r_piece_x.x, r_piece_x.y);
-		batch.end();
 
 		if(Gdx.input.isTouched())
 		{
 			touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			// we will just worry about the x piece now
-			r_piece_x.x = touchPos.x - 64 / 2;
-			r_piece_x.y = touchPos.x - 64 / 2;
-
+			pieceX.x = touchPos.x - 64 / 2;
+			pieceX.y = touchPos.y - 64 / 2;
 		}
 
-		if(r_piece_x.overlaps(r_board))
-		{
-			placePieceSound.play();
-		}
+		batch.begin();
+		batch.draw(boardImage, board.x, board.y);
+		batch.draw(pieceImage_x, pieceX.x, pieceX.y);
+		batch.end();
+
+
+
+
 
 	}
 	@Override
 	public void dispose()
 	{
-		image_o.dispose();
-		image_x.dispose();
-		boardTexture.dispose();
+		boardImage.dispose();
+		pieceImage_x.dispose();
 		placePieceSound.dispose();
 		roundOverSound.dispose();
 		gameMusic.dispose();
